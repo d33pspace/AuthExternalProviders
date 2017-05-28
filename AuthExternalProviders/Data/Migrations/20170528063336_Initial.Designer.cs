40dd1313c18cfd164362b4c33b5976c052551b40
Jimmy McWhinney <jimmy.mcw@live.com>
2017-05-28 14:34:49 +0800

    Add migrations.

--- AuthExternalProviders/Data/Migrations/20170528063336_Initial.Designer.cs
+++ AuthExternalProviders/Data/Migrations/20170528063336_Initial.Designer.cs
@@ -8,9 +8,10 @@ using AuthExternalProviders.Data;
 namespace AuthExternalProviders.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20170528063336_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.2")
