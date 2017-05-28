aee0294f31603124ae567ac2dc79f73c45c3d3ad
Jimmy McWhinney <jimmy.mcw@live.com>
2017-05-28 15:21:19 +0800

    Add Facebook authorization.

--- AuthExternalProviders/Startup.cs
+++ AuthExternalProviders/Startup.cs
@@ -86,6 +86,12 @@ namespace AuthExternalProviders
 
             // Add external authentication middleware below. To configure them please see https://go.microsoft.com/fwlink/?LinkID=532715
 
+            app.UseFacebookAuthentication(new FacebookOptions()
+            {
+                AppId = Configuration["Authentication:Facebook:AppId"],
+                AppSecret = Configuration["Authentication:Facebook:AppSecret"]
+            });
+
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
